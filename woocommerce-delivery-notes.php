d225c6c59d83049cfa336576602797463f774a30
mtmsujan <mtmsujon@gmail.com>
2024-07-08 16:44:39 +0600

    initial commit

---
+++ woocommerce-delivery-notes.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Print invoices & delivery notes for WooCommerce orders.
+ *
+ * Plugin Name: WooCommerce Print Invoice & Delivery Note
+ * Plugin URI: https://github.com/mtmsujan/woocommerce-delivery-note
+ * Description: Print Invoices & Delivery Notes for WooCommerce Orders. 
+ * Version: 1.0.0
+ * Author: Mtm Sujan
+ * Author URI: https://github.com/mtmsujan/woocommerce-delivery-note
+ * License: GPLv3 or later
+ * License URI: http://www.opensource.org/licenses/gpl-license.php
+ * Text Domain: woocommerce-delivery-note
+ * Domain Path: /languages
+ *
+ * Copyright 2024 Triggvy Gunderson
+ *		
+ *     This file is part of WooCommerce Print Invoices & Delivery Note,
+ *     a plugin for WordPress.
+ *
+ *     WooCommerce Print Invoice & Delivery Note is free software:
+ *     You can redistribute it and/or modify it under the terms of the
+ *     GNU General Public License as published by the Free Software
+ *     Foundation, either version 2 of the License, or (at your option)
+ *     any later version.
+ *
+ *     WooCommerce Print Invoice & Delivery Note is distributed in the hope that
+ *     it will be useful, but WITHOUT ANY WARRANTY; without even the
+ *     implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ *     PURPOSE. See the GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with WordPress. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+?>
